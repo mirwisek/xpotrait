c9768d102c83f5ec00b7b5382ad873bd757eb4f2
mirwisek <mirwise001@gmail.com>
2024-11-27 18:57:49 +0100

    Update the sys path for test_xportrait

--- core/api_run.py
+++ core/api_run.py
@@ -1,3 +1,8 @@
+import sys, os
+
+sys.path.insert(0, os.path.join(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'core'))
+
 from test_xportrait import main
 
 def generate_ai_video(
